c736ea5c80fa5a46bc75e37d10fe177d58ac09db
iabdalkarim <iabdalkarim@vmware.com>
2023-02-13 11:12:48 +0200

    Revert "Override execute method of AbstractInstallNodeDepsMojo class in InstallNodeDepsMojo in Abx and Vro plugins"
    This reverts commit 4e689ba347281badb53d3061c52160f6747b7491.

--- maven/plugins/abx/src/main/java/com/vmware/pscoe/maven/plugins/InstallNodeDepsMojo.java
+++ maven/plugins/abx/src/main/java/com/vmware/pscoe/maven/plugins/InstallNodeDepsMojo.java
@@ -15,71 +15,8 @@ package com.vmware.pscoe.maven.plugins;
  * #L%
  */
 
-import org.apache.commons.lang3.SystemUtils;
-import org.apache.maven.artifact.Artifact;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.*;
-
-import java.io.File;
-import java.util.LinkedList;
-import java.util.List;
 
 @Mojo(name = "install-node-deps", defaultPhase = LifecyclePhase.INITIALIZE, requiresDependencyResolution = ResolutionScope.RUNTIME_PLUS_SYSTEM)
 public class InstallNodeDepsMojo extends AbstractInstallNodeDepsMojo {
-	@Override
-	public void execute() throws MojoExecutionException, MojoFailureException {
-		boolean allTgzLibsResolved = true;
-		File nodeModules = new File(project.getBasedir(), "node_modules");
-		if(skipInstallNodeDeps)
-		{
-			if(nodeModules.exists())
-			{
-				getLog().info("Skipping the Dependency installation");
-				return;
-			}
-			else
-			{
-				getLog().info("Ignoring the flag skipInstallNodeDeps," +
-					"as node dependencies doesn't exist and are required for the successful build...");
-			}
-
-		}
-		if (!nodeModules.exists()) {
-			getLog().debug("node_modules doesn't exists. Creating it...");
-			nodeModules.mkdirs();
-		}
-
-		List<String> deps = new LinkedList<>();
-		String npmExec = SystemUtils.IS_OS_WINDOWS ? "npm.cmd" : "npm";
-		deps.add(npmExec);
-		deps.add("install");
-		for (Object o : project.getArtifacts()) {
-			Artifact a = (Artifact) o;
-			if ("tgz".equals(a.getType())) {
-				deps.add(a.getFile().getAbsolutePath());
-				allTgzLibsResolved = allTgzLibsResolved && a.isResolved();
-			}
-		}
-		if (!getLog().isDebugEnabled()) {
-			deps.add("--silent");
-		}
-
-		deps.add("--no-save");
-
-		if (!allTgzLibsResolved) {
-			getLog().debug("Not All .tgz plugins resolved. Executing 'mvn dependency:go-offline' first");
-			new ProcessExecutor()
-				.name("Going Offline")
-				.directory(project.getBasedir())
-				.command("mvn dependency:go-offline")
-				.execute(getLog());
-		}
-
-		new ProcessExecutor()
-			.name("Dependency installation")
-			.directory(project.getBasedir())
-			.command(deps)
-			.execute(getLog());
-	}
 }
