ed6495a4ec274b6e9d63d475b36fb0c9afa45c7d
nvodenicharski99 <nvodenicharski@vmware.com>
2023-02-23 15:43:50 +0200

    use mvn revision
    Signed-off-by: nvodenicharski99 <nvodenicharski@vmware.com>

--- typescript/polyglotpkg/src/strategies/nodejs.ts
+++ typescript/polyglotpkg/src/strategies/nodejs.ts
@@ -18,6 +18,21 @@ export class NodejsStrategy extends BaseStrategy {
     async packageProject() {
 
         const polyglotJson = await fs.readJSONSync(this.options.polyglotJson) as PlatformDefinition;
+
+/*-
+ * #%L
+ * polyglotpkg
+ * %%
+ * Copyright (C) 2023 VMware
+ * %%
+ * Build Tools for VMware Aria
+ * Copyright 2023 VMware, Inc.
+ * 
+ * This product is licensed to you under the BSD-2 license (the "License"). You may not use this product except in compliance with the BSD-2 License.  
+ * 
+ * This product may include a number of subcomponents with separate copyright notices and license terms. Your use of these subcomponents is subject to the terms and conditions of the subcomponent's license, as noted in the LICENSE file.
+ * #L%
+ */
         const tsconfigPath = path.join(this.options.actionBase, "tsconfig.json");
         const tmpTsconfigPath = this.options.mixed? path.join(this.options.actionBase, "tsconfig.tmp.json"): tsconfigPath;
         if (!fs.existsSync(tsconfigPath)) {
--- typescript/polyglotpkg/src/strategies/python.ts
+++ typescript/polyglotpkg/src/strategies/python.ts
@@ -70,6 +70,21 @@ export class PythonStrategy extends BaseStrategy {
      */
     async packageProject() {
         const polyglotJson = await fs.readJSONSync(this.options.polyglotJson) as PlatformDefinition;
+
+/*-
+ * #%L
+ * polyglotpkg
+ * %%
+ * Copyright (C) 2023 VMware
+ * %%
+ * Build Tools for VMware Aria
+ * Copyright 2023 VMware, Inc.
+ * 
+ * This product is licensed to you under the BSD-2 license (the "License"). You may not use this product except in compliance with the BSD-2 License.  
+ * 
+ * This product may include a number of subcomponents with separate copyright notices and license terms. Your use of these subcomponents is subject to the terms and conditions of the subcomponent's license, as noted in the LICENSE file.
+ * #L%
+ */
         this.phaseCb(Events.COMPILE_START);
         await this.compile(this.options.src, this.options.out);
         this.phaseCb(Events.COMPILE_END);
