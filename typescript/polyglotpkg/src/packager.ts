16ba7f212c9cd2406c39d5fb650ce3f4ff18a0e2
nvodenicharski99 <nvodenicharski@vmware.com>
2023-02-27 18:31:47 +0200

    all poms point to iac pom, all poms uses {revision} version
    Signed-off-by: nvodenicharski99 <nvodenicharski@vmware.com>

--- typescript/polyglotpkg/src/packager.ts
+++ typescript/polyglotpkg/src/packager.ts
@@ -24,6 +24,21 @@ export class Packager extends EventEmitter {
         // Collect list of actions included in the project
         const projectActions = await getProjectActions(this.options, <ActionType>this.options.env);
 
+/*-
+ * #%L
+ * polyglotpkg
+ * %%
+ * Copyright (C) 2023 VMware
+ * %%
+ * Build Tools for VMware Aria
+ * Copyright 2023 VMware, Inc.
+ * 
+ * This product is licensed to you under the BSD-2 license (the "License"). You may not use this product except in compliance with the BSD-2 License.  
+ * 
+ * This product may include a number of subcomponents with separate copyright notices and license terms. Your use of these subcomponents is subject to the terms and conditions of the subcomponent's license, as noted in the LICENSE file.
+ * #L%
+ */
+
         // Loop all actions found and execute the packager for each of them
         for (var i = 0; i < projectActions.length; i++) {
             const actionType = projectActions[i].actionType;
