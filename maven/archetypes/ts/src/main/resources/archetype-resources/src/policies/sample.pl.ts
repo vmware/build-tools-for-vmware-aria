c702c62b580bced9e3127d197aac117a00b1c5f9
Akash Ayyappan <48151852+akashayyappan@users.noreply.github.com>
2023-12-08 10:44:08 +0530

    modified policy sample file

--- maven/archetypes/ts/src/main/resources/archetype-resources/src/policies/sample.pl.ts
+++ maven/archetypes/ts/src/main/resources/archetype-resources/src/policies/sample.pl.ts
@@ -3,11 +3,25 @@ import { PolicyTemplate } from "vrotsc-annotations";
 @PolicyTemplate({
     name: "Sample Policy",
     path: "MyOrg/MyProject",
-    type: "AMQP:Subscription"
+    variables: {
+        sample: {
+            type: "string",
+            value: "",
+            description: "Description"
+        }
+    },
+    elements: {
+        sample: {
+            type: "AMQP:Subscription",
+            events: {
+                onMessage: "onMessage"
+            }
+        }
+    }
 })
 export class SamplePolicy {
-	onMessage(self: AMQPSubscription, event: any) {
+    onMessage(self: AMQPSubscription, event: any) {
         let message = self.retrieveMessage(event);
-		System.log(`Received message ${message.bodyAsText}`);
-	}
+        System.log(`Received message ${message.bodyAsText}`);
+    }
 }
