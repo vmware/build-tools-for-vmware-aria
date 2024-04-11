90c1f1761e43c40bef8b3e925199c414439f0cee
Yordan Nedelchev <ynedelchev@vmware.com>
2024-04-11 12:10:26 +0300

    1. Tests, 2. Changelog, 3. Update archetypes
    Signed-off-by: Yordan Nedelchev <ynedelchev@vmware.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -16,6 +16,7 @@
 * [artifact-manager] IAC-796 / Upgrade VCD archetype to support Angular 15
 * [vRA-NG] IAC-782 / vra-ng:pull : Added support for export of policies of the following types: Approval Policy, Day 2 Actions Policy, Deployment Limit Policy, Lease Policy, Resource Quota policy.
 * [artifact-manager] IAC-782 / vrealize:push : Added support for import of policies of the following types: Approval Policy, Day 2 Actions Policy, Deployment Limit Policy, Lease Policy, Resource Quota policy.
+* [artifact-manager] Export (Pull) Arai Automation Custom forms in a structured JSON, rather than in string encoded JSON, when stored together with the Custom Form metadata, thus making it more human friendly to operate with when stored in a repo.
 
 ### Fixes
 * [vro-types] Fix SSHSession 'error' and 'state' types
---
+++ common/artifact-manager/src/test/java/com/vmware/pscoe/iac/artifact/store/vrang/VraNgCustomFormTest.java
@@ -0,0 +1,174 @@
+package com.vmware.pscoe.iac.artifact.store.vrang;
+
+/*-
+ * #%L
+ * artifact-manager
+ * %%
+ * Copyright (C) 2023 - 2024 VMware
+ * %%
+ * Build Tools for VMware Aria
+ * Copyright 2023 VMware, Inc.
+ *
+ * This product is licensed to you under the BSD-2 license (the "License").
+ * You may not use this product except in compliance with the BSD-2 License.
+ *
+ * This product may include a number of subcomponents with separate copyright
+ * notices and license terms. Your use of these subcomponents is subject to the
+ * terms and conditions of the subcomponent's license, as noted in the
+ * LICENSE file.
+ * #L%
+ */
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import com.google.gson.JsonElement;
+import com.vmware.pscoe.iac.artifact.model.vrang.VraNgCustomForm;
+import com.vmware.pscoe.iac.artifact.model.vrang.VraNgCustomFormAndData;
+import java.io.IOException;
+
+public class VraNgCustomFormTest {
+    @Test
+    void testCustomFormConstruction() throws IOException {
+        // GIVEN
+        String jsonString = "{\n"
+        + "    \"layout\":{\n"
+        + "       \"pages\":[\n"
+        + "          {\n"
+        + "             \"id\":\"page_1\",\n"
+        + "             \"title\":\"General\",\n"
+        + "             \"sections\":[\n"
+        + "                {\n"
+        + "                   \"id\":\"section_project\",\n"
+        + "                   \"fields\":[\n"
+        + "                      {\n"
+        + "                         \"id\":\"project\",\n"
+        + "                         \"display\":\"dropDown\",\n"
+        + "                         \"signpostPosition\":\"right-middle\"\n"
+        + "                      }\n"
+        + "                   ]\n"
+        + "                },\n"
+        + "                {\n"
+        + "                   \"id\":\"section_deploymentName\",\n"
+        + "                   \"fields\":[\n"
+        + "                      {\n"
+        + "                         \"id\":\"deploymentName\",\n"
+        + "                         \"display\":\"textField\",\n"
+        + "                         \"signpostPosition\":\"right-middle\"\n"
+        + "                      }\n"
+        + "                   ]\n"
+        + "                },\n"
+        + "                {\n"
+        + "                   \"id\":\"section_0\",\n"
+        + "                   \"fields\":[\n"
+        + "                      {\n"
+        + "                         \"id\":\"numberOne\",\n"
+        + "                         \"display\":\"decimalField\",\n"
+        + "                         \"signpostPosition\":\"right-middle\"\n"
+        + "                      }\n"
+        + "                   ]\n"
+        + "                },\n"
+        + "                {\n"
+        + "                   \"id\":\"section_1\",\n"
+        + "                   \"fields\":[\n"
+        + "                      {\n"
+        + "                         \"id\":\"numberTwo\",\n"
+        + "                         \"display\":\"decimalField\",\n"
+        + "                         \"signpostPosition\":\"right-middle\"\n"
+        + "                      }\n"
+        + "                   ]\n"
+        + "                }\n"
+        + "             ],\n"
+        + "             \"state\":{\n"
+        + "                \n"
+        + "             }\n"
+        + "          }\n"
+        + "       ]\n"
+        + "    },\n"
+        + "    \"schema\":{\n"
+        + "       \"project\":{\n"
+        + "          \"label\":\"Project\",\n"
+        + "          \"type\":{\n"
+        + "             \"dataType\":\"string\",\n"
+        + "             \"isMultiple\":false\n"
+        + "          },\n"
+        + "          \"valueList\":{\n"
+        + "             \"id\":\"projects\",\n"
+        + "             \"type\":\"scriptAction\"\n"
+        + "          },\n"
+        + "          \"constraints\":{\n"
+        + "             \"required\":true\n"
+        + "          }\n"
+        + "       },\n"
+        + "       \"numberOne\":{\n"
+        + "          \"label\":\"Number One\",\n"
+        + "          \"type\":{\n"
+        + "             \"dataType\":\"decimal\",\n"
+        + "             \"isMultiple\":false\n"
+        + "          },\n"
+        + "          \"default\":1,\n"
+        + "          \"constraints\":{\n"
+        + "             \"required\":true\n"
+        + "          }\n"
+        + "       },\n"
+        + "       \"numberTwo\":{\n"
+        + "          \"label\":\"Number Two\",\n"
+        + "         \"type\":{\n"
+        + "             \"dataType\":\"decimal\",\n"
+        + "             \"isMultiple\":false\n"
+        + "          },\n"
+        + "          \"default\":2,\n"
+        + "          \"constraints\":{\n"
+        + "             \"required\":true\n"
+        + "          }\n"
+        + "       },\n"
+        + "       \"deploymentName\":{\n"
+        + "          \"label\":\"Deployment Name\",\n"
+        + "          \"type\":{\n"
+        + "             \"dataType\":\"string\",\n"
+        + "             \"isMultiple\":false\n"
+        + "          },\n"
+        + "          \"constraints\":{\n"
+        + "             \"required\":true,\n"
+        + "             \"max-value\":80\n"
+        + "          }\n"
+        + "       }\n"
+        + "    },\n"
+        + "    \"options\":{\n"
+        + "       \"externalValidations\":[\n"
+        + "          \n"
+        + "       ]\n"
+        + "    }\n"
+        + " }\n";
+
+        // WHEN
+        VraNgCustomForm restFormSource = new VraNgCustomForm("123",
+            "Some Name", jsonString, null,
+            "com.vmw.vro.workflow", "requestForm",
+            "requestForm", "ON", "JSON");
+        VraNgCustomFormAndData repoFormSource =
+        new VraNgCustomFormAndData("123",
+            "Some Name", null, null,
+            "com.vmw.vro.workflow", "requestForm",
+            "requestForm", "ON", "JSON");
+
+        repoFormSource.setForm(jsonString);
+        JsonElement form = repoFormSource.getForm();
+
+        new VraNgCustomForm(repoFormSource);
+        VraNgCustomFormAndData repoFormNew =
+        new VraNgCustomFormAndData(restFormSource);
+
+        JsonElement formnew = repoFormNew.getForm();
+
+        // THEN
+        assertTrue(form.isJsonObject());
+        assertTrue(!form.isJsonPrimitive());
+        assertTrue(form.getAsJsonObject().get("layout") != null);
+
+        assertTrue(formnew.isJsonObject());
+        assertTrue(!formnew.isJsonPrimitive());
+        assertTrue(formnew.getAsJsonObject().get("layout") != null);
+
+    }
+}
