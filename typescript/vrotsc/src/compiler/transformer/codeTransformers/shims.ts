6e32c21f25e2eb0be125bf23093694a39be88f9e
Venelin Bakalov <bakalovv@vmware.com>
2024-05-28 10:33:37 +0300

    Remove test log
    Signed-off-by: Venelin Bakalov <bakalovv@vmware.com>

--- typescript/vrotsc/src/compiler/transformer/codeTransformers/shims.ts
+++ typescript/vrotsc/src/compiler/transformer/codeTransformers/shims.ts
@@ -6,9 +6,9 @@
  * %%
  * Build Tools for VMware Aria
  * Copyright 2023 VMware, Inc.
- * 
+ *
  * This product is licensed to you under the BSD-2 license (the "License"). You may not use this product except in compliance with the BSD-2 License.
- * 
+ *
  * This product may include a number of subcomponents with separate copyright notices and license terms. Your use of these subcomponents is subject to the terms and conditions of the subcomponent's license, as noted in the LICENSE file.
  * #L%
  */
@@ -171,7 +171,6 @@ export function transformShims(sourceFile: ts.SourceFile, context: ScriptTransfo
                 case "ObjectConstructor.assign":
                     return shimStaticCall("objectAssign", node);
                 case "ObjectConstructor.setPrototypeOf":
-                    console.error("TEST!!!");
                     return shimStaticCall("objectSetPrototypeOf", node);
             }
         }
