3f464d7ba003edf6663c42eada493b414bdedd01
Stefan Genov <stefan.genov@broadcom.com>
2024-07-30 10:25:41 +0300

    test: fix typo Signed-off-by: Stefan Genov <stefan.genov@broadcom.com>

--- typescript/vrotsc/e2e/cases/canvas-items/waiting-timer-edge-2.wf.ts
+++ typescript/vrotsc/e2e/cases/canvas-items/waiting-timer-edge-2.wf.ts
@@ -1,7 +1,7 @@
 import { Workflow, In, WaitingTimerItem } from "vrotsc-annotations";
 
 @Workflow({
-	name: "Waitin Timer Edge",
+	name: "Waiting Timer Edge",
 	path: "VMware/PSCoE",
 	description: "Waiting timer will point to shouldGoHere with target",
 	attributes: {
