470db21fa1645b17076e287ca687157124b792d3
Mihail Penchev (c) <pmihail@vmware.com>
2024-08-21 17:50:13 +0300

    test: [385] Policy definition error - updated test case policy templates
    Signed-off-by: Mihail Penchev (c) <pmihail@vmware.com>

--- typescript/vrotsc/e2e/cases/all-content/policyTemplates/PolicyTemplateAmqpV1.pl.ts
+++ typescript/vrotsc/e2e/cases/all-content/policyTemplates/PolicyTemplateAmqpV1.pl.ts
@@ -1,18 +1,9 @@
 import { PolicyTemplate } from "vrotsc-annotations";
 
 @PolicyTemplate({
-	name: "Policy Template Amqp",
+	name: "Policy Template Amqp V1",
 	path: "MyOrg/MyProject",
 	type: "AMQP:Subscription",
-	templateVersion: "v2",
-	elements: {
-		AMQPSubscription: {
-			type: "AMQP:Subscription",
-			events: {
-				OnMessage: "onMessage"
-			}
-		}
-	}
 })
 export class PolicyTemplateAmqp {
 	onMessage(self: AMQPSubscription, event: any) {
