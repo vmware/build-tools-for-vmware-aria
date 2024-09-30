7e012c1e09d641c0dd6eb963edc36fae0b03034e
Stefan Genov <stefan.genov@broadcom.com>
2024-09-30 18:10:29 +0300

    fix: Added logger init for the AbstractVraNgDeleteStore Signed-off-by: Stefan Genov <stefan.genov@broadcom.com>
    Signed-off-by: Stefan Genov <stefan.genov@broadcom.com>

--- common/artifact-manager/src/main/java/com/vmware/pscoe/iac/artifact/store/vrang/AbstractVraNgDeleteStore.java
+++ common/artifact-manager/src/main/java/com/vmware/pscoe/iac/artifact/store/vrang/AbstractVraNgDeleteStore.java
@@ -1,7 +1,23 @@
 package com.vmware.pscoe.iac.artifact.store.vrang;
 
+/*-
+ * #%L
+ * artifact-manager
+ * %%
+ * Copyright (C) 2023 - 2024 VMware
+ * %%
+ * Build Tools for VMware Aria
+ * Copyright 2023 VMware, Inc.
+ * 
+ * This product is licensed to you under the BSD-2 license (the "License"). You may not use this product except in compliance with the BSD-2 License.
+ * 
+ * This product may include a number of subcomponents with separate copyright notices and license terms. Your use of these subcomponents is subject to the terms and conditions of the subcomponent's license, as noted in the LICENSE file.
+ * #L%
+ */
+
 import java.util.List;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.vmware.pscoe.iac.artifact.model.vrang.Identifiable;
 
@@ -17,6 +33,7 @@ public abstract class AbstractVraNgDeleteStore {
 	 * @TODO: Make it so no definition means delete everything if a flag is set
 	 */
 	public void deleteContent() {
+		Logger logger = LoggerFactory.getLogger(this.getClass());
 		List<Identifiable> serverResources = this.getAllServerContents();
 		List<String> items = this.getItemListFromDescriptor();
 
