c929c3bd68b40b07384ee9028edc0f7270e54410
Stefan Genov <stefan.genov@broadcom.com>
2024-10-14 09:14:22 +0300

    refactor(lint): Removed unused import Signed-off-by: Stefan Genov <stefan.genov@broadcom.com>

--- common/artifact-manager/src/main/java/com/vmware/pscoe/iac/artifact/rest/RestClientVraNgPrimitive.java
+++ common/artifact-manager/src/main/java/com/vmware/pscoe/iac/artifact/rest/RestClientVraNgPrimitive.java
@@ -21,7 +21,6 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.rmi.UnexpectedException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
