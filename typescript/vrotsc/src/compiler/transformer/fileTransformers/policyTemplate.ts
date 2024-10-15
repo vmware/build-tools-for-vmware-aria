vmware/build-tools-for-vmware-aria

commit d41da14b726a2743a83b5365039b343fc58c34a5

fix(policyTemplate)!: Better error if `templateVersion` missing Signed-off-by: Stefan Genov <stefan.genov@broadcom.com>

typescript/vrotsc/src/compiler/transformer/fileTransformers/policyTemplate.ts
@@ -118,10 +118,10 @@ export function getPolicyTemplateTransformer(file: FileDescriptor, context: File
 			name: classNode.name.text,
 			path: undefined,
 			tag: undefined,
-			type: "AMQP:Subscription",
+			type: "",
 			version: "1.0.0",
 			events: [],
-			templateVersion: V1,
+			templateVersion: "",
 			variables: {},
 			elements: {}
 		};
@@ -224,6 +224,8 @@ export function getPolicyTemplateTransformer(file: FileDescriptor, context: File
 	/**
 	 * This function processes a single decorator to fill in the policy template descriptor.
 	 *
+	 * If the templateVersion is not defined, will throw
+	 *
 	 * @param {PolicyTemplateDescriptor} policyTemplateInfo - The policy template info.
 	 * @param {ts.CallExpression} decoratorCallExp - The decorator call expression.
 	 */
@@ -254,10 +256,14 @@ export function getPolicyTemplateTransformer(file: FileDescriptor, context: File
 						initializer.properties
 							.filter((property: ts.PropertyAssignment) => !!property.initializer && ts.isStringLiteral(property.initializer))
 							.forEach((property: ts.PropertyAssignment) => {
-								policyTemplateInfo.schedule[getPropertyName(property.name)] = getText(property.initializer);
+								policyTemplateInfo.schedule[propName] = getText(property.initializer);
 							});
 						break;
 					}
+					// The default will technicall work, but we want to explicitly have this here for readability
+					case "templateVersion":
+						policyTemplateInfo.templateVersion = getText(initializer);
+						break;
 					case "type":
 						versionValidator.push(propName, null, V1, "use elements instead"); // no break!
 					default: {
@@ -269,6 +275,11 @@ export function getPolicyTemplateTransformer(file: FileDescriptor, context: File
 					}
 				}
 			});
+
+		if (policyTemplateInfo.templateVersion === "") {
+			throw new Error(`PolicyTemplate attribute 'templateVersion' is required.`);
+		}
+
 		// delayed validation until all attributes (incl. current template version) are known:
 		versionValidator.validate(policyTemplateInfo.templateVersion);
 	}

typescript/vrotsc/src/compiler/decorators.ts
@@ -213,7 +213,7 @@ export interface PolicyTemplateDescriptor {
 	version: string;
 	schedule?: PolicyTemplateScheduleDescriptor;
 	events: PolicyTemplateEventDescriptor[];
-	templateVersion?: string;
+	templateVersion: string;
 	variables?: Record<string, PolicyAttribute>;
 	elements?: Record<string, PolicyElement>;
 }

commit efab94679147410460120750fc88363f2c15d2ef

fix: Added versions in template, reverted a bug
Bug was introduced in d41da14b:

-    policyTemplateInfo.schedule[getPropertyName(property.name)] = getText(property.initializer);
+    policyTemplateInfo.schedule[propName] = getText(property.initializer);
Signed-off-by: Stefan Genov <stefan.genov@broadcom.com>

typescript/vrotsc/src/compiler/transformer/fileTransformers/policyTemplate.ts
@@ -256,7 +256,7 @@ export function getPolicyTemplateTransformer(file: FileDescriptor, context: File
 						initializer.properties
 							.filter((property: ts.PropertyAssignment) => !!property.initializer && ts.isStringLiteral(property.initializer))
 							.forEach((property: ts.PropertyAssignment) => {
-								policyTemplateInfo.schedule[propName] = getText(property.initializer);
+								policyTemplateInfo.schedule[getPropertyName(property.name)] = getText(property.initializer);
 							});
 						break;
 					}

typescript/vrotsc/e2e/cases/all-content/policyTemplates/PolicyTemplateAmqpV1.pl.ts
@@ -4,6 +4,7 @@ import { PolicyTemplate } from "vrotsc-annotations";
 	name: "Policy Template Amqp V1",
 	path: "MyOrg/MyProject",
 	type: "AMQP:Subscription",
+	templateVersion: "v1"
 })
 export class PolicyTemplateAmqp {
 	onMessage(self: AMQPSubscription, event: any) {

typescript/vrotsc/e2e/cases/all-content/policyTemplates/PolicyTemplateScheduleV1.pl.ts
@@ -3,6 +3,7 @@ import { PolicyTemplate } from "vrotsc-annotations";
 @PolicyTemplate({
 	name: "Policy Template Schedule V1",
 	path: "MyOrg/MyProject",
+	templateVersion: "v1",
 	schedule: {
 		periode: "every-days",
 		when: "11:12:00,12:00:00",