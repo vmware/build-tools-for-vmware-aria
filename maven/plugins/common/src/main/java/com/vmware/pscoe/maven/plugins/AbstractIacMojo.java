f21c20dddc24525f5a790714a84ab6f7049db152
Stefan Genov <stefan.genov@broadcom.com>
2025-01-16 12:24:10 +0200

    fix: Import for mojo Signed-off-by: Stefan Genov <stefan.genov@broadcom.com>

--- maven/plugins/common/src/main/java/com/vmware/pscoe/maven/plugins/AbstractIacMojo.java
+++ maven/plugins/common/src/main/java/com/vmware/pscoe/maven/plugins/AbstractIacMojo.java
@@ -16,6 +16,7 @@ package com.vmware.pscoe.maven.plugins;
 
 import com.vmware.pscoe.iac.artifact.configuration.*;
 import com.vmware.pscoe.iac.artifact.aria.automation.configuration.ConfigurationVraNg;
+import com.vmware.pscoe.iac.artifact.aria.operations.configuration.ConfigurationVrops;
 import com.vmware.pscoe.iac.artifact.model.PackageType;
 import com.vmware.pscoe.iac.artifact.rest.RestClientFactory;
 import com.vmware.pscoe.iac.artifact.rest.RestClientVro;
