7e42c471227d1a4196554311f7fa3b3de3c0e38c
Stefan Genov <stefan.genov@broadcom.com>
2025-01-28 08:51:53 +0200

    style(lint): Remove unused import Signed-off-by: Stefan Genov <stefan.genov@broadcom.com>

--- common/artifact-manager/src/main/java/com/vmware/pscoe/iac/artifact/aria/automation/store/VraNgContentSharingPolicyStore.java
+++ common/artifact-manager/src/main/java/com/vmware/pscoe/iac/artifact/aria/automation/store/VraNgContentSharingPolicyStore.java
@@ -28,7 +28,6 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.util.StringUtils;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
