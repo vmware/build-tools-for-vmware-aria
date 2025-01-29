82e1bc7a0a678a28ad6f1b888675598584a60279
Stefan Genov <stefan.genov@broadcom.com>
2025-01-29 14:01:20 +0200

    Update common/artifact-manager/src/main/java/com/vmware/pscoe/iac/artifact/aria/automation/store/VraNgContentSharingPolicyStore.java
    Co-authored-by: Venelin Bakalov <bakalovv@vmware.com>
    Signed-off-by: Stefan Genov <sgenov@vmware.com>

--- common/artifact-manager/src/main/java/com/vmware/pscoe/iac/artifact/aria/automation/store/VraNgContentSharingPolicyStore.java
+++ common/artifact-manager/src/main/java/com/vmware/pscoe/iac/artifact/aria/automation/store/VraNgContentSharingPolicyStore.java
@@ -449,7 +449,7 @@ public class VraNgContentSharingPolicyStore extends AbstractVraNgStore {
 	}
 
 	/**
-	 * Sanitizes the give JSON object policy by removing properties that should not
+	 * Sanitizes the given JSON object policy by removing properties that should not
 	 * be stored.
 	 *
 	 * The `projectId` must not be removed, as it controls wether a policy is
