3fa82ee64a6b307a92571ae52d7a862187e392ea
Venelin Bakalov <bakalovv@vmware.com>
2025-08-22 11:22:07 +0300

    Add test case. Update documentation trailing spaces.

--- typescript/vrotsc/e2e/cases/canvas-items/user-interaction.wf.ts
+++ typescript/vrotsc/e2e/cases/canvas-items/user-interaction.wf.ts
@@ -1,33 +1,40 @@
-import { Workflow, In, Out, Item, RootItem, UserInteractionItem } from "vrotsc-annotations";
+import { In, Item, Out, RootItem, UserInteractionItem, Workflow } from "vrotsc-annotations";
 
 @Workflow({
-	name: "User Interaction",
-	path: "VMware/PSCoE",
-	description: "Adding user interaction parameters"
+    name: "User Interaction",
+    path: "VMware/PSCoE",
+    description: "Adding user interaction parameters"
 })
 export class UserInteractionWorkflow {
-	@Item({ target: "userInteraction1Enter", exception: "" })
-	@RootItem()
-	public start() {
-		System.log("Starting workflow");
-	}
+    @Item({ target: "userInteraction1Enter", exception: "" })
+    @RootItem()
+    public start() {
+        System.log("Starting workflow");
+    }
 
-	@UserInteractionItem({
-		target: "userInteraction2Enter"
-	})
-	public userInteraction1Enter() {
-		System.log(`Start user interaction 1`);
-	}
+    @UserInteractionItem({
+        target: "userInteraction2Enter"
+    })
+    public userInteraction1Enter() {
+        System.log(`Start user interaction 1`);
+    }
 
-	@UserInteractionItem({
-		target: "userInteractionExit"
-	})	
-	public userInteraction2Enter(@In security_assignees: LdapUser[], @In security_assignee_groups: LdapGroup[], @In security_group: LdapGroup, @In timeout_date?: Date, @In userInteractionAnswer?: string) {
-		System.log(`User interaction component answered with '${userInteractionAnswer}'`);
-	}
+    @UserInteractionItem({
+        target: "userInteraction3Enter"
+    })
+    public userInteraction2Enter(@In security_assignees: LdapUser[], @In security_assignee_groups: LdapGroup[], @In security_group: LdapGroup, @In timeout_date?: Date, @In userInteractionAnswer?: string) {
+        System.log(`User interaction component answered with '${userInteractionAnswer}'`);
+    }
 
-	@Item({ target: "end" })
-	public userInteractionExit(@Out timeoutDate: Date) {
-		System.log(`User Interaction exit on ${timeoutDate?.toUTCString()}`);
-	}
+    @UserInteractionItem({
+        target: "userInteractionExit"
+    })
+    public userInteraction3Enter() {
+        System.log(`Start user interaction 3`);
+    }
+
+    @Item({ target: "end" })
+    public userInteractionExit(@Out timeoutDate: Date) {
+        System.log(`User Interaction exit on ${timeoutDate?.toUTCString()}`);
+    }
 }
