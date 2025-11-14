8c0b0d8fc23a8d59460d84812ce53a2d665b8f5f
Venelin Bakalov <bakalovv@vmware.com>
2025-11-14 12:49:04 +0200

    Remove unneeded mock

--- common/artifact-manager/src/test/java/com/vmware/pscoe/iac/artifact/helpers/GeneralMocks.java
+++ common/artifact-manager/src/test/java/com/vmware/pscoe/iac/artifact/helpers/GeneralMocks.java
@@ -14,67 +14,65 @@
  */
 package com.vmware.pscoe.iac.artifact.helpers;
 
+import static org.mockito.Mockito.when;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.mockito.Mockito;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import static org.mockito.Mockito.*;
-
 public class GeneralMocks {
 	/**
 	 * Response entity mock with a status code of 200 and no headers
 	 *
-	 * @param	body - Body of response
-	 * @param	<T> - Response type
+	 * @param body - Body of response
+	 * @param <T>  - Response type
 	 *
-	 * @return	ResponseEntity<T>
+	 * @return ResponseEntity<T>
 	 */
-	public static <T> ResponseEntity<T> mockResponseEntity( T body ) {
-		return mockResponseEntity( body, 200 );
+	public static <T> ResponseEntity<T> mockResponseEntity(T body) {
+		return mockResponseEntity(body, 200);
 	}
 
 	/**
 	 * Response entity mock with no headers
 	 *
-	 * @param	body - Body of response
-	 * @param	statusCode - status code of the response
-	 * @param	<T> - Response type
+	 * @param body       - Body of response
+	 * @param statusCode - status code of the response
+	 * @param <T>        - Response type
 	 *
-	 * @return	ResponseEntity<T>
+	 * @return ResponseEntity<T>
 	 */
-	public static <T> ResponseEntity<T> mockResponseEntity( T body, int statusCode ) {
-		Map<String, String> headers	= new HashMap<>();
+	public static <T> ResponseEntity<T> mockResponseEntity(T body, int statusCode) {
+		Map<String, String> headers = new HashMap<>();
 
-		return mockResponseEntity( body, statusCode, headers );
+		return mockResponseEntity(body, statusCode, headers);
 	}
 
 	/**
 	 * Mocks a response entity
 	 * You can pass a status code and it will create a HttpStatus object.
 	 *
-	 * @return	ResponseEntity
+	 * @return ResponseEntity
 	 */
 	public static <T> ResponseEntity<T> mockResponseEntity(
-		T body,
-		int statusCode,
-		Map<String, String> headers
-	) {
-		ResponseEntity<T> response	= Mockito.mock( ResponseEntity.class );
-		HttpStatus statusCodeObj	= HttpStatus.valueOf( statusCode );
-		HttpHeaders responseHeaders	= new HttpHeaders();
+			T body,
+			int statusCode,
+			Map<String, String> headers) {
+		ResponseEntity<T> response = Mockito.mock(ResponseEntity.class);
+		HttpStatus statusCodeObj = HttpStatus.valueOf(statusCode);
+		HttpHeaders responseHeaders = new HttpHeaders();
 
-		for ( Map.Entry<String, String> entry : headers.entrySet() ) {
-			responseHeaders.set( entry.getKey(), entry.getValue() );
+		for (Map.Entry<String, String> entry : headers.entrySet()) {
+			responseHeaders.set(entry.getKey(), entry.getValue());
 		}
 
-		when( response.getBody() ).thenReturn( body );
-		when( response.getStatusCode() ).thenReturn( statusCodeObj );
-		when( response.getStatusCodeValue() ).thenReturn( statusCode );
-		when( response.getHeaders() ).thenReturn( responseHeaders );
+		when(response.getBody()).thenReturn(body);
+		when(response.getStatusCode()).thenReturn(statusCodeObj);
+		when(response.getHeaders()).thenReturn(responseHeaders);
 
 		return response;
 	}
